e9dcfcd9b6efa04fceeb98cb813b138684b41262
Balabi94 <orfejorfeev@gmail.com>
2022-02-21 00:15:37 +0600

    Сверстана секция About, шапке вешается класс scroll-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport hight, add class scroll-header
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
